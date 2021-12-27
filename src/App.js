62ba99f19091c624e3c47da2cc4a88ca54824663
backwards-rat-race <backwards-rat-race@users.noreply.github.com>
2021-12-27 23:38:45 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+import React, {useEffect, useState} from "react";
+import wretch from "wretch";
+import MarkdownEditor from "rich-markdown-editor";
+
+export function App() {
+    const [note, setNote] = useState();
+
+    useEffect(async () => {
+        const { id } = await wretch('http://localhost:3000/v1/create-note-request').post().json();
+        setNote({ id })
+    }, []);
+
+    if (!note) {
+        return <div>Loading...</div>
+    }
+
+    const onSaveClick = async () => {
+        // Setting this to null triggers the loading state
+        setNote(null);
+        setNote(await save(ref, note));
+    }
+
+    const ref = React.createRef();
+    return (
+        <div>
+            <div className="header">
+                Sync Notes
+                <button role="button" onClick={onSaveClick}>Save</button>
+            </div>
+            <div className="editor-container">
+                <MarkdownEditor value={note.data} ref={ref}/>
+            </div>
+        </div>
+    );
+
+}
+
+async function save(editorRef, note) {
+    const value = editorRef.current.value();
+    if (note.data === undefined) {
+        return await wretch('http://localhost:3000/v1/note')
+            .post({
+                id: note.id,
+                data: value
+            })
+            .json();
+    } else {
+        return await wretch(`http://localhost:3000/v1/note/${note.id}`)
+            .put({
+                id: note.id,
+                data: value
+            })
+            .json();
+    }
+}
