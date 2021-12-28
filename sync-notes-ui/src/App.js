0cfb57ee18fd4c4a3cfa384a2a28f6f8f2a06de7
backwards-rat-race <backwards-rat-race@users.noreply.github.com>
2021-12-28 02:19:04 +0000

    Correct dynamic URL for production build vs development build of UI

--- sync-notes-ui/src/App.js
+++ sync-notes-ui/src/App.js
@@ -3,11 +3,13 @@ import React, {useEffect, useState} from "react";
 import wretch from "wretch";
 import MarkdownEditor from "rich-markdown-editor";
 
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
 export function App() {
     const [note, setNote] = useState();
 
     useEffect(async () => {
-        const { id } = await wretch('http://localhost:3000/api/v1/create-note-request').post().json();
+        const { id } = await wretch(`${BASE_URL}/v1/create-note-request`).post().json();
         setNote({ id })
     }, []);
 
@@ -39,14 +41,14 @@ export function App() {
 async function save(editorRef, note) {
     const value = editorRef.current.value();
     if (note.data === undefined) {
-        return await wretch('http://localhost:3000/api/v1/note')
+        return await wretch(`${BASE_URL}/v1/note`)
             .post({
                 id: note.id,
                 data: value
             })
             .json();
     } else {
-        return await wretch(`http://localhost:3000/api/v1/note/${note.id}`)
+        return await wretch(`${BASE_URL}/v1/note/${note.id}`)
             .put({
                 id: note.id,
                 data: value
